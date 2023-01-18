4ad17088b554a9ed83c60ee68ea8c374ef2083e0
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-01-18 11:41:33 +0530

    Day 27:(Task) Created Beautiful Landing Page 🚀

---
+++ Day-27/Task/script.js
@@ -0,0 +1,75 @@
+
+var india = document.getElementById("india");
+var australia = document.getElementById("australia");
+var japan = document.getElementById("japan");
+var china = document.getElementById("china");
+var displayTime = document.getElementById("time");
+
+var interval; // variable to store the id returned by setInterval
+var timeZone = "en-IN"; // default selected locale
+
+
+india.addEventListener("click", function(event){
+    updateTime(event);
+});
+australia.addEventListener("click", function(event){
+    updateTime(event);
+});
+japan.addEventListener("click", function(event){
+    updateTime(event);
+});
+china.addEventListener("click", function(event){
+    updateTime(event);
+});
+
+
+
+function updateTime(event) {
+    if(event && event.target){
+        // event && event.target means if event is not null and event.target is not null
+        var target = event.target;        
+        switch(target.id){
+            case 'india':
+                selectedTimeZone='Asia/Kolkata';
+                shortForm='IN';
+                break;
+            case 'australia':
+                selectedTimeZone= 'Australia/Sydney';
+                shortForm='AU';
+                break;
+            case 'japan':
+                selectedTimeZone= 'Asia/Tokyo';
+                shortForm='JP';
+                break;
+            case 'china':
+                selectedTimeZone= 'Asia/Shanghai';
+                shortForm='CH';
+                break;
+        }
+    }
+    //* reffered: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl
+    var currentTime = new Date();
+    // const indianTime = () => {
+        // document.getElementById("indianTime").innerHTML = new Date().toLocaleString(
+        //     "en-US", 
+        //     {
+        //         timeZone: "Asia/Kolkata",
+        //         timeStyle:'medium', 
+        //         hourCycle:"h12"
+        //     }
+        // );
+    // }
+    // indianTime()
+    // setInterval(indianTime,1000);
+    var timeString = new Intl.DateTimeFormat(timeZone, {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true,
+        timeZone:selectedTimeZone
+    }).format(currentTime);
+    displayTime.innerHTML = timeString+" <strong>"+shortForm+"</strong>";
+    clearInterval(interval);
+    interval = setInterval(updateTime, 1000);
+}
+updateTime();
