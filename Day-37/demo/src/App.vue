36f17afa3e1b34deedf2c1f9d417df5adfefdcec
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-02-06 10:46:29 +0530

    Day 37: Started with  Vue.js 🚀

---
+++ Day-37/demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HeaderDemo/>
+  </div>
+</template>
+
+<script>
+import HeaderDemo from './components/HeaderDemo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderDemo
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
