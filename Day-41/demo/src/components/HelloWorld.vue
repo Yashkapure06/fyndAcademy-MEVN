759904a1398482eac41c5904143e6385c105f361
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-02-06 10:48:56 +0530

    Day 41:  Vue.js filter 🚀

---
+++ Day-41/demo/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- <p v-size="'small'">This is paragraph</p> 
+    <p v-size>This is paragraph</p> 
+    <p v-size="'big'">This is paragraph</p> 
+    <span  v-changeStyle>This is span</span><br/>
+    <span  v-changeStyle="{color:'purple'}">This is span</span> -->
+
+    <!-- <h1>{{ message }}</h1> -->
+    <h1>{{ changeToUpperCase() }}</h1>
+    <p>marks in % {{ marks | perConv }}</p>
+<!-- Filter -->
+    <h1>{{ str | toUpperCase | isPalindrome }}</h1>
+
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      message:"my name is yash kapure",
+      marks:7.5,
+      str:"leVeL"
+    }
+  },
+  methods:{
+    changeToUpperCase(){
+      return this.message.toUpperCase()/;
+    }
+  },
+}
+</script>
+
---
+++ Day-41/demo/src/main.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+//creating custom directives
+Vue.directive("size",{
+  bind(el,binding){
+    if(binding.value =='small'){
+      el.style.fontSize = "30px";
+    }else if(binding.value =='big'){
+      el.style.fontSize = "100px";
+    }else{
+      el.style.fontSize = "50px";
+    }
+  }
+})
+//creating custom directive changing color
+Vue.directive("changeStyle",{
+  bind(el,binding){
+    if(binding.value && binding.value.color){
+    // if(binding.value!=null){
+      console.log(binding.value);
+      console.log(binding.value.color);
+
+      el.style.color = binding.value.color;
+      el.style.fontSize = "50px";
+    }
+    else{
+      el.style.color = "blue";
+      el.style.fontSize = "50px";
+    }
+  }
+})
+
+Vue.filter('perConv', function(val){
+  return val*9.5 +"%";
+})
+
+Vue.filter('isPalindrome', function(str){
+  if(str===''){
+    return "Empty String";
+  }else  if(str.split('').reverse().join('') === str){
+    return "Palindrome";
+  }
+  else{
+    return "Not Palindrome";
+  }
+})
+
+Vue.filter("toUpperCase", function(str){
+  return str.toUpperCase();
+})
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
